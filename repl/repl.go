52432d9c988d3706632a69f5ca2e5bc9f02ffc35
kawamataryou <ba068082@gmail.com>
2020-06-14 04:36:05 +0900

    [from now] 2020/06/14 04:36:05
    diff --git a/main.go b/main.go
    new file mode 100644
    index 0000000..d9d2494
    --- /dev/null
    +++ b/main.go
    @@ -0,0 +1,18 @@
    +package main
    +
    +import (
    +	"fmt"
    +	"os"
    +	"os/user"
    +	"github.com/kawamataryo/go-monkey/repl"
    +)
    +
    +func main() {
    +	user, err := user.Current()
    +	if err != nil {
    +		panic(err)
    +	}
    +	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
    +	fmt.Printf("Feel free to type in commands\n")
    +	repl.Start(os.Stdin, os.Stdout)
    +}
    diff --git a/repl/repl.go b/repl/repl.go
    index 2de31e9..aee8625 100644
    --- a/repl/repl.go
    +++ b/repl/repl.go
    @@ -14,16 +14,24 @@ func Start(in io.Reader, out io.Writer) {
     	scanner := bufio.NewScanner(in)
    
     	for {
    +		// プロンプトを表示
     		fmt.Print(PROMPT)
    +		// 標準入力を待機
     		scanned := scanner.Scan()
     		if !scanned {
     			return
     		}
    
    +		// 入力文字をlineに代入
     		line := scanner.Text()
    +
    +		// 字句解析機へ
     		l := lexer.New(line)
    
    -		// forの書き方がよく分からない
    +		// 軸解解析機で検証
    +		// 初期化でtokにl.nextToken()の取得結果を代入
    +		// 条件式でeofを判定
    +		// 増減式でtok = l.nextToken()を実行
     		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
     			fmt.Printf("%+v\n", tok)
     		}

--- repl/repl.go
+++ repl/repl.go
@@ -14,16 +14,24 @@ func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	for {
+		// プロンプトを表示
 		fmt.Print(PROMPT)
+		// 標準入力を待機
 		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
 
+		// 入力文字をlineに代入
 		line := scanner.Text()
+
+		// 字句解析機へ
 		l := lexer.New(line)
 
-		// forの書き方がよく分からない
+		// 軸解解析機で検証
+		// 初期化でtokにl.nextToken()の取得結果を代入
+		// 条件式でeofを判定
+		// 増減式でtok = l.nextToken()を実行
 		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
 			fmt.Printf("%+v\n", tok)
 		}
