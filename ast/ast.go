127fabe970da7e29aa2d092b8d2d1dd3708b9cad
kawamataryou <ba068082@gmail.com>
2020-06-20 04:49:09 +0900

    [from now] 2020/06/20 04:49:08
    diff --git a/ast/ast.go b/ast/ast.go
    index bc3c46a..d1b4ced 100644
    --- a/ast/ast.go
    +++ b/ast/ast.go
    @@ -2,24 +2,36 @@ package ast
    
     import "github.com/kawamataryo/go-monkey/token"
    
    +// 全てのノードが実装する
     type Node interface {
    +	// トークンのリテラル値を返す。123とか、{}とか文字列
    +	// デバックとテストのために用いる
     	TokenLiteral() string
     }
    
    +// 一部のノードが実装する
    +// Statement = 文？
     type Statement interface {
     	Node
    +	// ダミーメソッド？
     	statementNode()
     }
    
    +// 一部のノードが実装する
    +// Expression = 表現？式？
     type Expression interface {
     	Node
    +	// ダミーメソッド？
     	expressionNode()
     }
    
    +// 最初のノード
    +// Statementのスライスを持つ
     type Program struct {
     	Statements []Statement
     }
    
    +// トークンリテラルを出力するための関数
     func (p *Program) TokenLiteral() string {
     	if len(p.Statements) > 0 {
     		return p.Statements[0].TokenLiteral()
    @@ -28,17 +40,27 @@ func (p *Program) TokenLiteral() string {
     	}
     }
    
    +// Letを表すASTのノードの実装
     type LetStatement struct {
    +	// 軸解析で得たトークン （typeとliteralを持つ）
     	Token token.Token
    +	// 束縛する変数名
     	Name  *Identifier
    +	// 束縛される式
     	value Expression
     }
    
    +// letステートメント構造体が持つダミーメソッド
     func (ls *LetStatement) statementNode() {}
    +// letステートメント構造体が持つトークンリテラルの出力メソッド
    +// デバックテストに使われる
     func (ls *LetStatement) TokenLiteral() string {
     	return ls.Token.Literal
     }
    
    +// 束縛の識別子を保持するためのもの。
    +// Expressionインターフェイスを実装する。
    +// なぜなら、let以外の場所では、識別子は値を生成するから。（変数からの値の取り出しのように）
     type Identifier struct {
     	Token token.Token
     	Value string
    diff --git a/reading-memo.md b/reading-memo.md
    index 3a45b29..ed56244 100644
    --- a/reading-memo.md
    +++ b/reading-memo.md
    @@ -5,12 +5,21 @@
     - 期待された構造に従っているかも検証する
     - パーサージェネレーター。yacc, bison, ANTLR、何らかの言語の記述を受け取って、その出力としてパーサーを生成する。便利なものもあるんだな。
    
    -トップダウン構文解析と、ボトムアップ構文解析。
    
    -- 式は値を生成し、文はしない。
    +構文解析の戦略については大きく分けて2つ。トップダウン構文解析と、ボトムアップ構文解析。
    +トップダウン構文解析は、ルートノードから構築していく。
    +ボトムアップ構文解析は、その逆
    +
    +トップダウン構文解析にもいくつか種類がある。
    +- 再帰下降構文解析
    +- アーリー法
    +- 予測的構文解析
    +
    +式は値を生成し、文はしない。
    +
    +letとreturnの構文解析。
    +letを構文解析するとは？ => 変数束縛を表現する？
    
    -AST = プログラムを意味付けしたデータ構造？
    -入力された文字列をを解析して作られる
    
     # 字句解析
     ソースコードからトークン列への変換を字句解析という。

--- ast/ast.go
+++ ast/ast.go
@@ -2,24 +2,36 @@ package ast
 
 import "github.com/kawamataryo/go-monkey/token"
 
+// 全てのノードが実装する
 type Node interface {
+	// トークンのリテラル値を返す。123とか、{}とか文字列
+	// デバックとテストのために用いる
 	TokenLiteral() string
 }
 
+// 一部のノードが実装する
+// Statement = 文？
 type Statement interface {
 	Node
+	// ダミーメソッド？
 	statementNode()
 }
 
+// 一部のノードが実装する
+// Expression = 表現？式？
 type Expression interface {
 	Node
+	// ダミーメソッド？
 	expressionNode()
 }
 
+// 最初のノード
+// Statementのスライスを持つ
 type Program struct {
 	Statements []Statement
 }
 
+// トークンリテラルを出力するための関数
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -28,17 +40,27 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+// Letを表すASTのノードの実装
 type LetStatement struct {
+	// 軸解析で得たトークン （typeとliteralを持つ）
 	Token token.Token
+	// 束縛する変数名
 	Name  *Identifier
+	// 束縛される式
 	value Expression
 }
 
+// letステートメント構造体が持つダミーメソッド
 func (ls *LetStatement) statementNode() {}
+// letステートメント構造体が持つトークンリテラルの出力メソッド
+// デバックテストに使われる
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
 
+// 束縛の識別子を保持するためのもの。
+// Expressionインターフェイスを実装する。
+// なぜなら、let以外の場所では、識別子は値を生成するから。（変数からの値の取り出しのように）
 type Identifier struct {
 	Token token.Token
 	Value string
